cee9fc556f65181c4c9679bad839b15d201d5647
Alex de Lima Bassi <albbassi@gmail.com>
2024-04-19 22:38:13 -0300

    Update __init__.py

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -2,10 +2,10 @@
 from schemas.cliente import ClienteSchema, ClienteViewSchema, ClientesApresentacao, ClienteApresenta, ClienteBuscaSchema, ClientesListagemSchema
 
 #schemas para apresentação dos dados de contrato
-from schemas.contrato import ContratoSchema, ContratoViewSchema, ContratoApresenta, ContratosApresentacao, ContratoBuscaSchema, ContratosListagemSchema
+#from schemas.contrato import ContratoSchema, ContratoViewSchema, ContratoApresenta, ContratosApresentacao, ContratoBuscaSchema, ContratosListagemSchema
 
 #schema para apresentação das informações de itens de contrato
-from schemas.item_contrato import ItemContratoSchema, ItemContratoViewSchema, ItensContratosApresentacao, ItemContratoApresenta, ItemContratoBuscaSchema, ItensContratoListagemSchema
+#from schemas.item_contrato import ItemContratoSchema, ItemContratoViewSchema, ItensContratosApresentacao, ItemContratoApresenta, ItemContratoBuscaSchema, ItensContratoListagemSchema
 
 #schema para apresentação dos dados de log
 from schemas.error import ErrorSchema
